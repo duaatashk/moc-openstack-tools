4ebc587ac5bd8147bdd40f359641a5ed8f3b8311
duaatashk <duaat@bu.edu>
2017-04-26 11:23:05 -0400

    in line 13 in test_message.py, i removed 'from message import BadEmailRecipient' to avoid circular dependent imports

--- tests/test_message.py
+++ tests/test_message.py
@@ -10,7 +10,7 @@ TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
 sys.path.insert(0, PROJECT_DIR)
 
-from message import Message, TemplateMessage, BadEmailRecipient
+from message import Message, TemplateMessage
 
 
 def test_template():
