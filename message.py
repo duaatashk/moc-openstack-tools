6a3e5d9b8fc4ca291df9e6626643d81a1ff9967b
duaatashk <duaat@bu.edu>
2017-04-26 16:51:40 -0400

    trying to fix the importing error

--- message.py
+++ message.py
@@ -17,7 +17,7 @@ import string
 import smtplib
 from email.mime.text import MIMEText
 from moc_utils import get_absolute_path
-from moc_exceptions import BadEmailRecipient
+from .moc_exceptions import BadEmailRecipient
 
 '''
 class BadEmailRecipient(Exception):
